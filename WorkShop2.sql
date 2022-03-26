b756bd8586ddf1f1e4c41eb83036f21c9cefc7ab
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-03-26 17:25:52 +0300

    Update WorkShop2.sql

--- WorkShop2.sql
+++ WorkShop2.sql
@@ -1,1 +1,5 @@
-SELECT * FROM Employees e LEFT JOIN Orders o ON e.EmployeeID = o.EmployeeID WHERE o.EmployeeID is null
+--- Hiç satış yapamayan personel var mı? Kimlerdir?
+SELECT * FROM Employees e 
+LEFT JOIN Orders o 
+ON e.EmployeeID = o.EmployeeID 
+WHERE o.EmployeeID is null
