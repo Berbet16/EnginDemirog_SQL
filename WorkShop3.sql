d348a1d6980a8d613661bd13243669d7a2ada178
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-03-26 17:36:24 +0300

    Create WorkShop3.sql

---
+++ WorkShop3.sql
@@ -0,0 +1,10 @@
+HANGİ ÜRÜNDEN KAÇ TANE SATMIŞIZ?
+HANGİ KATEGORİDEN KAÇ TANE SATMIŞIZ?
+
+SELECT p.ProductName , COUNT(*) as ADET 
+FROM Products p 
+INNER JOIN
+[Order Details] o 
+ON p.ProductID = o.ProductID
+GROUP BY p.ProductName
+ORDER BY p.ProductName
