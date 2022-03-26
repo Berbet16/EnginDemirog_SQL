d329ad0a1d46ca13d1c991e5f6554cca991f0989
Betül Berna Soylu <63414835+Berbet16@users.noreply.github.com>
2022-03-26 17:43:36 +0300

    Update WorkShop4.sql

--- WorkShop4.sql
+++ WorkShop4.sql
@@ -1,1 +1,9 @@
 HANGİ KATEGORİDEN KAÇ TANE SATMIŞIZ?
+
+SELECT c.CategoryName, COUNT(*) AS Adet 
+FROM Products p 
+INNER JOIN Categories c 
+ON p.CategoryID = c.CategoryID
+INNER JOIN [Order Details] od
+ON od.ProductID = p.ProductID
+GROUP BY c.CategoryName 
